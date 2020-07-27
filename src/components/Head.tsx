40d6866d87798caafa7b458a17af290722b41336
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-07-27 21:05:18 +0200

    fix(lhci): fix uses-rel-preconnect error

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -10,15 +10,16 @@ export const Head = ({
 }) => (
 	<>
 		<Helmet>
-			<link
-				href="https://fonts.googleapis.com/css2?family=Bitter&family=Catamaran:wght@300&amp;display=swap"
-				rel="stylesheet"
-			></link>
 			<title>{title}</title>
 			<meta name="description" content={description} />
 			<html lang="en" />
 			<link rel="icon" type="image/svg+xml" href={withPrefix('/favicon.svg')} />
 			<link rel="apple-touch-icon" href={withPrefix('/favicon-ios.png')}></link>
+			<link rel="preconnect" href="https://fonts.googleapis.com" />
+			<link
+				href="https://fonts.googleapis.com/css2?family=Bitter&family=Catamaran:wght@300&amp;display=swap"
+				rel="stylesheet"
+			></link>
 		</Helmet>
 	</>
 )
