5176e5445c193084088f36cd518d4846a6ca5380
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-08-05 09:35:37 +0200

    fix: add attribution for ✌  icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,29 +3,46 @@ import styled from 'styled-components'
 
 import HeartIcon from 'feather-icons/dist/icons/heart.svg'
 import GitHubIcon from 'feather-icons/dist/icons/github.svg'
+import VictoryIcon from './noun_victory_394398.svg'
 import { SiteMetaData } from '../templates/page'
 
 const StyledFooter = styled.footer`
 	padding: 4rem;
 	svg {
-		&.heart {
-			color: #f11748;
-		}
 		display: inline-block;
 		margin-bottom: -8px;
 		padding: 0 0.25rem;
 	}
-	p {
-		text-align: center;
-		margin-bottom: 2rem;
-		&.dim {
-			margin-bottom: 0;
-			opacity: 0.75;
-		}
-	}
 	a {
 		color: inherit;
 	}
+`
+
+const P = styled.p`
+	text-align: center;
+	margin-bottom: 2rem;
+`
+
+const Dimmed = styled(P)`
+	opacity: 0.75;
+	margin-bottom: 0;
+`
+
+const StyledVictoryIcon = styled(VictoryIcon)`
+	height: 24px;
+	width: 24px;
+	fill: #fff;
+	margin-bottom: -5px;
+	opacity: 0.75;
+`
+
+const StyledHeartIcon = styled(HeartIcon)`
+	color: #f11748;
+`
+
+const Attributions = styled.aside`
+	margin-top: 4rem;
+	margin-bottom: 2rem;
 `
 
 export const Footer = ({
@@ -34,10 +51,19 @@ export const Footer = ({
 	siteMetaData: SiteMetaData
 }) => (
 	<StyledFooter>
-		<p>
-			Made with <HeartIcon className="heart" /> by Refugees in Moria.
-		</p>
-		<p className="dim">
+		<P>
+			Made with <StyledHeartIcon /> by Refugees in Moria.
+		</P>
+		<Attributions>
+			<Dimmed>
+				<small>
+					<StyledVictoryIcon /> icon created by Sergey Demushkin
+					<br />
+					from the Noun Project.
+				</small>
+			</Dimmed>
+		</Attributions>
+		<Dimmed>
 			&copy;2020{' '}
 			<a
 				href="https://distributeaid.org/"
@@ -47,8 +73,8 @@ export const Footer = ({
 				Distribute Aid
 			</a>
 			. All rights reserved.
-		</p>
-		<p className="dim">
+		</Dimmed>
+		<Dimmed>
 			<a
 				href={gitHubUrl}
 				target="_blank"
@@ -57,6 +83,6 @@ export const Footer = ({
 			>
 				<GitHubIcon />
 			</a>
-		</p>
+		</Dimmed>
 	</StyledFooter>
 )
