3429dd643ac2577ecb0308930c29d4bd3e34c8c9
Markus Tacker <m@coderbyheart.com>
2020-08-07 12:52:42 +0200

    feat: load non critical styles asynchronously (#27)

--- critical-css.js
+++ critical-css.js
@@ -1,8 +1,22 @@
+/**
+ * This script extracts the critical CSS and adds it as the only style tag in
+ * the index.html's head. Other style tags in the head are converted to
+ * asynchronously loaded styles.
+ */
+
 const minimalcss = require('minimalcss')
 const path = require('path')
-const { promises: fs } = require('fs')
-
+const fs = require('fs')
+const JSDOM = require('jsdom').JSDOM
 const crypto = require('crypto')
+
+const id = () =>
+	crypto
+		.randomBytes(Math.ceil(8 * 0.5))
+		.toString('hex')
+		.slice(0, 8)
+
+console.error('Extracting critical styles:')
 
 minimalcss
 	.minimize({
@@ -20,15 +34,63 @@ minimalcss
 		},
 	})
 	.then(async ({ finalCss }) => {
-		const id = crypto
-			.randomBytes(Math.ceil(8 * 0.5))
-			.toString('hex')
-			.slice(0, 8)
 		const indexFile = path.join(process.cwd(), 'public', `index.html`)
-		let index = await fs.readFile(indexFile, 'utf-8')
-		index = index.replace(
-			'<head>',
-			`<head><style type="text/css">/* Critical CSS */ ${finalCss}</style>`,
+		const index = fs.readFileSync(indexFile, 'utf-8')
+		const originalSize = fs.statSync(indexFile).size
+
+		const jsdom = new JSDOM(index)
+		const { window } = jsdom
+		const head = window.document.getElementsByTagName('head')[0]
+		const body = window.document.getElementsByTagName('body')[0]
+
+		// Convert existing style tags to be loaded asynchronously
+
+		const extracted = []
+
+		head.childNodes.forEach(async (el) => {
+			if (el.tagName !== 'STYLE') return
+			// Save CSS to a file
+			const styleFile = `extracted-${id()}.css`
+			const styleLocation = path.join(process.cwd(), 'public', styleFile)
+			fs.writeFileSync(styleLocation, el.textContent)
+			console.error(
+				'-',
+				`${styleFile} written:`,
+				`${fs.statSync(styleLocation).size} byte`,
+			)
+			extracted.push(styleFile)
+			// Remove style tag
+			el.remove()
+		})
+
+		// Add extracted using JS calls so they load asynchronously
+		const script = window.document.createElement('script')
+		script.type = 'text/javascript'
+		script.defer = true
+		script.innerHTML =
+			`const loadAsync = (src) =>
+				((d) => {
+					var x = d.createElement('link')
+					var y = d.getElementsByTagName('script')[0]
+					x.rel = 'stylesheet'
+					x.href = src
+					y.parentNode.insertBefore(x, y)
+				})(document)
+			;` + extracted.map((s) => `loadAsync('./${s}');`).join('\n')
+		body.appendChild(script)
+
+		// Add critical CSS as style element
+		const critical = window.document.createElement('style')
+		critical.type = 'text/css'
+		critical.innerHTML = `/* Critical CSS */ ${finalCss}`
+		head.appendChild(critical)
+
+		const updatedIndex = jsdom.serialize()
+		fs.writeFileSync(indexFile, updatedIndex)
+		console.error(
+			'-',
+			`index.html written:`,
+			`${fs.statSync(indexFile).size - originalSize} bytes`,
 		)
-		await fs.writeFile(indexFile, index)
+		console.log('Done extracting critical styles.')
 	})
